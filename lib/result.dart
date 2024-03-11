411328ca3f98b2a1b1a887ebd31cdadf711b466c
Mohsin <mohsinnaqvi606@gmail.com>
2024-03-11 18:29:41 +0500

    screen shot

--- lib/result.dart
+++ lib/result.dart
@@ -10,6 +10,7 @@ class ResultView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Image.memory(list),
     );
   }
